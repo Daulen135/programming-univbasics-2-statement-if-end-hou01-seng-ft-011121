ba81e3e329673be0b72a0403203283357e71b255
Daulen Zhangabylov <daulenz@gmail.com>
2020-12-16 08:34:58 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,10 @@
-current_time = Time.now
-current_time = current_time.to_i
- 
-if current_time % 2 == 0
+current_time=Time.now 
+current_time=current_time.to_i 
+if current_time.even?
   puts "Even!"
 else
   puts "Odd!"
 end
-
 
 
 
